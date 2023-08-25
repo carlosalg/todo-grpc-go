33417af94538c25fb8386efb869bf45bc58d6fda
carlos <cabetolg98@gmail.com>
2023-08-25 17:46:01 -0500

    added Delete method and make some changes in the other test

--- internal/database/tests/db_test.go
+++ internal/database/tests/db_test.go
@@ -19,35 +19,52 @@ import (
 // }
 
 func TestCreateTodo(t *testing.T) {
+	t.Run("TestCreate", func(t *testing.T) {
+		repo, err := todorepo.NewTodoRepository()
+		assert.NoError(t, err)
 
-	repo, err := todorepo.NewTodoRepository()
-	assert.NoError(t, err)
+		todo := todorepo.Todo{
+			ID:        1,
+			Title:     "Test CreateTodo",
+			Completed: false,
+		}
+		err = repo.CreateTodo(todo)
+		assert.NoError(t, err)
 
-	todo := todorepo.Todo{
-		ID:        1,
-		Title:     "Test Todo",
-		Completed: false,
-	}
-	err = repo.CreateTodo(todo)
-	assert.NoError(t, err)
-
-	createdTodo, err := repo.GetTodoByID(1)
-	assert.NoError(t, err)
-	assert.Equal(t, todo, createdTodo)
+		createdTodo, err := repo.GetTodoByID(1)
+		assert.NoError(t, err)
+		assert.Equal(t, todo, createdTodo)
+	})
 }
 
 func TestGetTodoByID(t *testing.T) {
-	repo, err := todorepo.NewTodoRepository()
-	assert.NoError(t, err)
+	t.Run("TestGet", func(t *testing.T) {
+		repo, err := todorepo.NewTodoRepository()
+		assert.NoError(t, err)
 
-	err = repo.CreateTodo(todorepo.Todo{ID: 1, Title: "Test Todo", Completed: false})
-	assert.NoError(t, err)
+		err = repo.CreateTodo(todorepo.Todo{ID: 2, Title: "Test GetTodo", Completed: false})
+		assert.NoError(t, err)
 
-	todo, err := repo.GetTodoByID(1)
-	assert.NoError(t, err)
+		todo, err := repo.GetTodoByID(2)
+		assert.NoError(t, err)
 
-	expectedTodo := todorepo.Todo{ID: 1, Title: "Test Todo", Completed: false}
-	assert.Equal(t, expectedTodo, todo)
+		expectedTodo := todorepo.Todo{ID: 2, Title: "Test GetTodo", Completed: false}
+		assert.Equal(t, expectedTodo, todo)
+	})
+}
+
+func TestDeleteTodo(t *testing.T) {
+	t.Run("TestDelete", func(t *testing.T) {
+		repo, err := todorepo.NewTodoRepository()
+		assert.NoError(t, err)
+
+		todoToDelete := todorepo.Todo{ID: 3, Title: "Test DeleteTodo", Completed: false}
+		err = repo.CreateTodo(todoToDelete)
+		assert.NoError(t, err)
+
+		err = repo.DeleteTodo(3)
+		assert.NoError(t, err)
+	})
 }
 
 func cleanupTestData() error {
@@ -60,16 +77,16 @@ func cleanupTestData() error {
 
 func TestMain(m *testing.M) {
 	db.InitDB()
+	_, err := db.DB.Exec("ALTER TABLE todos AUTO_INCREMENT = 1")
+	if err != nil {
+		fmt.Printf("Error resetting auto_increment: %v\n", err)
+	}
+
 	result := m.Run()
 
 	cleanupErr := cleanupTestData()
 	if cleanupErr != nil {
 		fmt.Printf("Error cleaning up test data: %v\n", cleanupErr)
-	}
-
-	_, err := db.DB.Exec("ALTER TABLE todos AUTO_INCREMENT = 1")
-	if err != nil {
-		fmt.Printf("Error resetting auto_increment: %v\n", err)
 	}
 
 	db.CloseDB()
--- internal/database/todo_repo/todo_repository.go
+++ internal/database/todo_repo/todo_repository.go
@@ -28,7 +28,6 @@ func NewTodoRepository() (*TodoRepository, error) {
 
 func (r *TodoRepository) CreateTodo(todo Todo) error {
 	query := "INSERT INTO todos (title, completed) VALUES (?, ?)"
-
 	_, err := r.db.Exec(query, todo.Title, todo.Completed)
 	if err != nil {
 		return err
@@ -39,7 +38,6 @@ func (r *TodoRepository) CreateTodo(todo Todo) error {
 func (r *TodoRepository) GetTodoByID(id int32) (Todo, error) {
 	query := "SELECT id, title, completed FROM todos WHERE id = ?"
 	row := r.db.QueryRow(query, id)
-
 	var todo Todo
 	err := row.Scan(&todo.ID, &todo.Title, &todo.Completed)
 	if err != nil {
@@ -49,4 +47,23 @@ func (r *TodoRepository) GetTodoByID(id int32) (Todo, error) {
 		return todo, fmt.Errorf("GetTodoByID %d: %v", id, err)
 	}
 	return todo, nil
+}
+
+func (r *TodoRepository) DeleteTodo(id int32) error {
+	query := "DELETE FROM todos WHERE id = ?"
+	result, err := r.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("DeleteTodo %d: no Todo found", id)
+	}
+
+	return nil
 }
